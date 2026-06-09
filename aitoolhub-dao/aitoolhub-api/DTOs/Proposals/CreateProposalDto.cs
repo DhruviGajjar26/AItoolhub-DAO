b43da99b7f0977ef30905c31ff38914c3b17292b
Polar <connectgajjardhruvi@gmail.com>
2026-06-09 11:33:24 +0530

    Delete aitoolhub-dao directory

--- aitoolhub-dao/aitoolhub-api/DTOs/Proposals/CreateProposalDto.cs
+++
@@ -1,8 +0,0 @@
-namespace AIToolHub.API.DTOs.Proposals {
-    public class CreateProposalDto {
-        public string Title { get; set; } = string.Empty;
-        public string Description { get; set; } = string.Empty;
-        public string Category { get; set; } = "Tools";
-        public decimal BudgetRequest { get; set; }
-    }
-}
--- aitoolhub-dao/aitoolhub-api/DTOs/Tools/OcrRequest.cs
+++
@@ -1,6 +0,0 @@
-namespace AIToolHub.API.DTOs.Tools {
-    public class OcrRequest {
-        public string? Base64Image { get; set; }
-        public string? Language { get; set; } = "eng";
-    }
-}
